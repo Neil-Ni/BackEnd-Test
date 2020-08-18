426f83f616d4ca6eb51b7e7bac37c78745ee52a8
David Louie <dlouie1492@gmail.com>
2020-08-18 18:52:32 -0400

    add indexes

--- db/migrate/20200818220133_create_join_table_shop_book.rb
+++ db/migrate/20200818220133_create_join_table_shop_book.rb
@@ -1,8 +1,8 @@
 class CreateJoinTableShopBook < ActiveRecord::Migration[6.0]
   def change
     create_join_table :shops, :books do |t|
-      # t.index [:shop_id, :book_id]
-      # t.index [:book_id, :shop_id]
+      t.index [:shop_id, :book_id]
+      t.index [:book_id, :shop_id]
       t.boolean :sold, default: false, index: true
     end
   end
