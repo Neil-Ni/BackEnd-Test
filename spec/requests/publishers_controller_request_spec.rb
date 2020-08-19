f1cca96b9ee2e83f153f6ac1ad86a6e2bef04a12
David Louie <dlouie1492@gmail.com>
2020-08-19 19:45:28 -0400

    add publishers controller tests

---
+++ spec/requests/publishers_controller_request_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe PublishersController, type: :request do
+  before(:each) do
+    @publisher = create(:publisher)
+    3.times {create(:shop)}
+    3.times {@publisher.books << create(:book)}
+    Book.all.each do |book|
+      Shop.all.each do |shop|
+        rand(2..4).times {shop.books << book}
+      end
+    end
+  end
+
+  describe "GET #show" do
+    it "returns a shop inventory of its books" do
+      get publisher_path(@publisher.id)
+
+      expect(response).to be_successful
+    end
+
+    it "returns a JSON object" do
+      get publisher_path(@publisher.id)
+      data = eval(response.body)
+      expect(data.class).to eq(Hash)
+    end
+  end
+end
