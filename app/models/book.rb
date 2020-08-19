8f6589a21043732805cadb6db78d500c1f0f5a2f
David Louie <dlouie1492@gmail.com>
2020-08-18 20:00:40 -0400

    Set up associations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,6 @@
 class Book < ApplicationRecord
+  belongs_to :publisher
+  has_many :books_shops
+  alias_attribute :stock, :books_shops
+  has_many :shops, through: :books_shops
 end
--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,16 @@
 class Shop < ApplicationRecord
+  has_many :books_shops
+  alias_attribute :stock, :books_shops
+  has_many :books, through: :books_shops
+  has_many :publishers, through: :books
+
+  def sell(book_id)
+    book = self.books_shops.find_by(book: book_id)
+    if book
+      book.sold = true
+      book.save
+    else
+      return false
+    end
+  end
 end
