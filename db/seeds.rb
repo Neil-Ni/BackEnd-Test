8113c67ad54e5801e5f41283c3038b5c4ebeb405
David Louie <dlouie1492@gmail.com>
2020-08-18 20:08:37 -0400

    Create seeds file to populate database

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Publisher.delete_all
+Shop.delete_all
+Book.delete_all
+
+10.times do
+  Publisher.create(name: Faker::App.name)
+end
+
+10.times do
+  Shop.create(name: "#{Faker::Company.name} #{Faker::Company.industry}")
+end
+
+Publisher.all.each do |publisher|
+  10.times do
+    book = publisher.books.create(title: Faker::Hipster.sentence(word_count: 3))
+    20.times do
+      shop = Shop.find(Shop.pluck(:id).sample(1)[0])
+      shop.books << book
+      shop.sell(book.id) if rand(2) % 2 == 0
+    end
+  end
+end
