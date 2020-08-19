5fa0f2b89e1cf17df98f44e911cc61bb72527f02
David Louie <dlouie1492@gmail.com>
2020-08-18 22:58:38 -0400

    add note for improvement

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,6 +2,6 @@ class ShopsController < ApplicationController
   def update
     shop = Shop.find(params[:id])
     sold = shop.sell(params[:book_id], params[:count])
-    render json: {success: sold}
+    render json: {success: sold} ### sub out for error code?
   end
 end
