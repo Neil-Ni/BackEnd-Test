5f1c69980dd8b0bfc5fe3f471fe0d30bfec09a0c
David Louie <dlouie1492@gmail.com>
2020-08-18 22:48:01 -0400

    set up purchasing route and action

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -1,2 +1,7 @@
 class ShopsController < ApplicationController
+  def update
+    shop = Shop.find(params[:id])
+    sold = shop.sell(params[:book_id], params[:count])
+    render json: {success: sold}
+  end
 end
