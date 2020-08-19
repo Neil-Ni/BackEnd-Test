61104d841e5ea9b169e9c99c8baf0f4122facf48
David Louie <dlouie1492@gmail.com>
2020-08-18 20:09:35 -0400

    add primary key to join table to simplify updates when selling books

--- db/migrate/20200818220133_create_join_table_shop_book.rb
+++ db/migrate/20200818220133_create_join_table_shop_book.rb
@@ -1,6 +1,7 @@
 class CreateJoinTableShopBook < ActiveRecord::Migration[6.0]
   def change
     create_join_table :shops, :books do |t|
+      t.primary_key :id
       t.index [:shop_id, :book_id]
       t.index [:book_id, :shop_id]
       t.boolean :sold, default: false, index: true
