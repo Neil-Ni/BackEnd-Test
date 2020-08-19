4c1b0ce1f57fc68b8ecc4d07b6f409381e06c178
David Louie <dlouie1492@gmail.com>
2020-08-18 20:07:19 -0400

    Add methods to collect book stock data

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -4,6 +4,19 @@ class Shop < ApplicationRecord
   has_many :books, through: :books_shops
   has_many :publishers, through: :books
 
+  def books_sold
+    stock.where(sold: true)
+  end
+
+  def books_in_stock
+    in_stock = []
+    books.each {|book| in_stock << {id: book.id, title: book.title, copies_in_stock: copies_in_stock(book.id).count}}
+  end
+
+  def copies_in_stock(book_id)
+    stock.where(book_id: book_id, sold: false)
+  end
+
   def sell(book_id)
     book = self.books_shops.find_by(book: book_id)
     if book
