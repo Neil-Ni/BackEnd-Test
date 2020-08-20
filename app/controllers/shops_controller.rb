f842d9a13f5227340b56f712a538a1b1e7b583dc
David Louie <dlouie1492@gmail.com>
2020-08-20 19:12:22 -0400

    refactor return code for book selling

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -2,10 +2,7 @@ class ShopsController < ApplicationController
   def update
     shop = Shop.find(params[:id])
     sold = shop.sell(params[:book_id], params[:count].to_i)
-    if sold
-      head 200
-    else
-      head 400
+    head (sold ? 200 : 400)
     end
   end
 end
