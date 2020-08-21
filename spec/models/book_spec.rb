3e4e3ab2c0f4d038f2e18e89fba21decabd81b5b
David Louie <dlouie1492@gmail.com>
2020-08-20 20:25:36 -0400

    add association testing

--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @publisher = create(:publisher)
+    3.times {create(:shop)}
+    3.times {@publisher.books << create(:book)}
+    Book.all.each do |book|
+      Shop.all.each do |shop|
+        rand(2..4).times {shop.books << book}
+      end
+    end
+    @shop = Shop.first
+    @book = @shop.books.first
+  end
+
+  describe "has" do
+    it "shops" do
+      expect(@book.shops.count).to eq(BooksShop.where(book_id: @book.id).pluck(:shop_id).count)
+    end
+
+    it "publishers" do
+      expect(@book.publisher).to eq(Publisher.find(@book.publisher_id))
+    end
+  end
 end
