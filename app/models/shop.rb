b402970124295f899cf2bad0edbd773c49a59041
David Louie <dlouie1492@gmail.com>
2020-08-20 20:03:33 -0400

    allow preloaded counts to store data for multiple publishers

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -5,7 +5,6 @@ class Shop < ApplicationRecord
   has_many :publishers, -> {distinct}, through: :books
 
   def publisher_data(publisher_id)
-    stock_counts(publisher_id)
     {id: self.id,
      name: self.name,
      books_sold_count: stock.where(sold: true).count,
@@ -13,13 +12,18 @@ class Shop < ApplicationRecord
   end
 
   def books_in_stock(publisher_id)
-    books.where(publisher_id: publisher_id, sold: false).uniq.map {|book| {id: book.id, title: book.title, copies_in_stock: @counts[book.id]}}
+    stock_counts(publisher_id) unless @counts
+    books.where(publisher_id: publisher_id,).uniq.map {|book| {id: book.id, title: book.title, copies_in_stock: @counts[publisher_id][book.id]}}
   end
 
   def stock_counts(publisher_id)
-    return @counts if @counts
-    @counts = Hash.new(0)
-    books_shops.where(book_id: books.where(publisher_id: publisher_id).pluck(:id).uniq).each {|book| @counts[book.book_id] += 1}
+    return @counts[publisher_id] if @counts
+    @counts = Hash.new
+    books_shops.where(book_id: books.where(publisher_id: publisher_id).pluck(:id).uniq).each do |book|
+      @counts[publisher_id] ||= Hash.new(0)
+      @counts[publisher_id][book.book_id] += 1
+    end
+    @counts[publisher_id]
   end
 
   def inventory_count(book_id)
