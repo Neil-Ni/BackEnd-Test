84ec4df7f8bbbca0c31b9c3faca25b3b6904c3d1
David Louie <dlouie1492@gmail.com>
2020-08-20 20:22:55 -0400

    remove leftover end

--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,6 +3,5 @@ class ShopsController < ApplicationController
     shop = Shop.find(params[:id])
     sold = shop.sell(params[:book_id], params[:count].to_i)
     head (sold ? 200 : 400)
-    end
   end
 end
