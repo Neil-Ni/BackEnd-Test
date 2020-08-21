a7be0ccd0cc6ce2e83575316a01c400e052d1c42
David Louie <dlouie1492@gmail.com>
2020-08-20 20:17:05 -0400

    add tests for shop model

--- spec/models/shop_spec.rb
+++ spec/models/shop_spec.rb
@@ -1,5 +1,55 @@
 require 'rails_helper'
 
 RSpec.describe Shop, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @publisher = create(:publisher)
+    3.times {create(:shop)}
+    3.times {@publisher.books << create(:book)}
+    Book.all.each do |book|
+      Shop.all.each do |shop|
+        rand(2..4).times {shop.books << book}
+      end
+    end
+    @shop = Shop.first
+    @book = @shop.books.first
+  end
+
+  describe "has" do
+    it "books" do
+      expect(@shop.books.length).to eq(BooksShop.where(shop_id: @shop.id).pluck(:book_id).uniq.count)
+    end
+
+    it "books in stock" do
+      in_stock = @shop.publisher_data(@publisher.id)
+      expect(in_stock.keys).to eq([:id, :name, :books_sold_count, :books_in_stock])
+      expect(in_stock[:books_sold_count]).to eq(@shop.stock.where(sold: true).count)
+    end
+
+    it "publishers" do
+      expect(@shop.publishers.length).to eq(@shop.books.pluck(:publisher_id).uniq.count)
+    end
+  end
+
+  describe "it" do
+    it "sells a book" do
+      count = @shop.inventory_count(@book.id)
+      sold = @shop.sell(@book.id)
+      expect(sold).to be(true)
+      expect(@shop.inventory_count(@book.id)).to eq(count - 1)
+    end
+
+    it "sells multiple books" do
+      count = @shop.inventory_count(@book.id)
+      sold = @shop.sell(@book.id, 2)
+      expect(sold).to be(true)
+      expect(@shop.inventory_count(@book.id)).to eq(count - 2)
+    end
+
+    it "doesn't sell more books than it has" do
+      count = @shop.inventory_count(@book.id)
+      sold = @shop.sell(@book.id, count + 1)
+      expect(sold).to be(false)
+      expect(@shop.inventory_count(@book.id)).to eq(count)
+    end
+  end
 end
